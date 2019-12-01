388f214b2b8ec4df867f9445ed8762df0d5a0036
Luis Carlin <luiscarlin@gmail.com>
2019-12-01 14:39:05 -0500

    clean up a bit

--- 1.js
+++ 1.js
@@ -2,9 +2,8 @@ const main = () => {
   let lines = require('fs')
     .readFileSync('./1.in', 'utf8')
     .split('\n')
-    .filter(n => n);
-
-  lines = lines.map(Number);
+    .filter(n => n)
+    .map(Number);
 
   console.log('part 1', lines.map(calculateFuel).reduce(sum));
   console.log('part 2', lines.map(num => getRecursiveFuel(num)).reduce(sum));
@@ -21,7 +20,7 @@ const getRecursiveFuel = (num, sum = 0) => {
 const sum = (a, b) => a + b;
 
 if (require.main == module) {
-  console.time('timer');
+  console.time('main');
   main();
-  console.timeEnd('timer');
+  console.timeEnd('main');
 }
