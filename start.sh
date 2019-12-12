e5cbcc23e25ec7dad4293676454fffd76404b42e
Luis Carlin <luiscarlin@gmail.com>
2019-12-12 07:14:11 -0500

    echo messages

--- start.sh
+++ start.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-dirname="day-${1}"
+num=${1}
+dirname="day-${num}"
 
-echo "starting ${dirname}"
+echo "starting day ${num}"
 
 cp -r day-n ${dirname}
 mv "${dirname}/day-n.test.js" "${dirname}/${dirname}.test.js"
 
+echo 'created'
 ls -l ${dirname}
+
+echo 'commit'
+git add ${dirname}
+git commit -m "auto start day ${num}"
 
 echo 'done!'
