104e0d3d59f90874896e691d53263c712818bd40
Luis Carlin <luiscarlin@gmail.com>
2019-12-14 21:55:03 -0500

    part 1 day11 done

--- day-11/part1.js
+++ day-11/part1.js
@@ -1,9 +1,71 @@
-const main = input => {
-  return input;
+/*
+inputs:
+- 0 => robot on black panel
+- 1 => robot on white panel
+
+outpus:
+- 0 => paint the panel black
+- 1 => paint the panel white
+- 0 => turn left 90 degrees
+- 1 => turn right 90 degrees
+
+
+- after turn, move exacly 1 panel
+- start facing up
+- Do not restart the Intcode computer inside the robot during this process.
+- start with all black panels
+
+- black panels = .
+- white panels = #
+*/
+
+const IntCode = require('./intcode');
+
+const DEFAULT_PANEL_COLOR = 0;
+
+const main = instructions => {
+  let panels = {};
+  const intCode = new IntCode(instructions);
+
+  const position = { x: 0, y: 0, dx: 0, dy: -1 };
+
+  while (true) {
+    let currentPanelColor =
+      panels[[position.x, position.y]] || DEFAULT_PANEL_COLOR;
+
+    let color = intCode.execute(currentPanelColor);
+    let turn = intCode.execute();
+
+    if (intCode.terminated) {
+      break;
+    }
+
+    if ((panels[[position.x, position.y]] || DEFAULT_PANEL_COLOR) !== color) {
+      panels[[position.x, position.y]] = color;
+    }
+
+    const rotate = turn === 0 ? 'left' : 'right';
+
+    const dx = po ? (rotate === 'left' ? position.dy : position.dy * -1) : 0;
+    const dy =
+      position.dy === 0
+        ? rotate === 'left'
+          ? position.dx * -1
+          : position.dx
+        : 0;
+
+    position.dx = dx;
+    position.dy = dy;
+
+    position.x += position.dx;
+    position.y += position.dy;
+  }
+
+  return Object.keys(panels).length;
 };
 
 if (require.main === module) {
-  const input = require('fs').readFileSync(`${__filename}/input.txt`);
+  const input = require('fs').readFileSync(`${__dirname}/input.txt`, 'utf8');
   console.log('part 1', main(input));
 }
 
