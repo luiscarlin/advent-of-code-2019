cb2f148bc9fb0542cc99076e0c0cd4831733b656
Luis Carlin <luiscarlin@gmail.com>
2019-12-15 10:08:17 -0500

    start part 1 day 12

--- day-12/part1.js
+++ day-12/part1.js
@@ -1,4 +1,43 @@
 const main = input => {
+  const something = input
+    .split('\n')
+    .map(line => line.slice(1, -1))
+    .map(item => item.split(',').map(item => item.trim().slice(2)));
+
+  let moon1 = {
+    x: something[0][0],
+    y: something[0][1],
+    z: something[0][2],
+    dx: 0,
+    dy: 0,
+    dz: 0,
+  };
+  let moon2 = {
+    x: something[1][0],
+    y: something[1][1],
+    z: something[1][2],
+    dx: 0,
+    dy: 0,
+    dz: 0,
+  };
+  let moon3 = {
+    x: something[2][0],
+    y: something[2][1],
+    z: something[2][2],
+    dx: 0,
+    dy: 0,
+    dz: 0,
+  };
+  let moon4 = {
+    x: something[3][0],
+    y: something[3][1],
+    z: something[3][2],
+    dx: 0,
+    dy: 0,
+    dz: 0,
+  };
+
+  console.log(something);
   return input;
 };
 
