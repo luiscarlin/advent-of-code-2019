ec7c3be2077f333d5e23bab9f6730cdad8cf531f
Luis Carlin <luiscarlin@gmail.com>
2019-12-17 16:41:09 -0500

    day 14 part 1 in progress

--- day-14/part1.js
+++ day-14/part1.js
@@ -1,10 +1,39 @@
 const main = input => {
-  return input;
+  const formulas = getFormulas(input);
+
+  console.log(formulas);
+};
+
+const getFormulas = lines => {
+  return lines
+    .map(line => line.split(/=>|,/))
+    .reduce((acc, elementList) => {
+      const [num, name] = elementList
+        .pop()
+        .trim()
+        .split(' ');
+
+      acc[[num, name]] = [];
+
+      elementList.forEach(otherElement => {
+        const [num1, name1] = otherElement.trim().split(' ');
+        acc[[num, name]].push(`${num1},${name1}`);
+      });
+
+      return acc;
+    }, []);
 };
 
 if (require.main === module) {
-  const input = require('fs').readFileSync(`${__dirname}/input.txt`, 'utf8');
+  const input = require('fs')
+    .readFileSync(`${__dirname}/input.txt`, 'utf8')
+    .split('\n');
   console.log('part 1', main(input));
 }
 
 module.exports = main;
+
+// {
+//   '8,PHBMP':  ['3,JQFM', '5,QMQB'],
+//   '8,PHBMP':  ['3,JQFM', '5,QMQB'],
+// }
