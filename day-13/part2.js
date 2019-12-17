51325f76bddf2a5dae53d5bdc625f3ebb0e0fa83
Luis Carlin <luiscarlin@gmail.com>
2019-12-16 22:08:48 -0500

    print map

--- day-13/part2.js
+++ day-13/part2.js
@@ -1,21 +1,68 @@
 const IntcodeComputer = require('./IntcodeComputer');
 
+const print = grid => {
+  const keys = Object.keys(grid).map(key => key.split(','));
+
+  const xList = keys.map(coord => +coord[0]);
+  const yList = keys.map(coord => +coord[1]);
+
+  const maxX = Math.max(...xList);
+  const minX = Math.min(...xList);
+  const maxY = Math.max(...yList);
+  const minY = Math.min(...yList);
+
+  for (let y = minY; y <= maxY; y++) {
+    for (let x = minX; x <= maxX; x++) {
+      const value = grid[[x, y]];
+      process.stdout.write(`${value}`);
+
+      // if (value === 0) {
+      //   process.stdout.write(' ');
+      // } else {
+      //   process.stdout.write('#');
+      // }
+    }
+    console.log();
+  }
+};
 const main = instructions => {
   let computer = new IntcodeComputer(instructions);
 
+  let grid = {};
+
   while (!computer.terminated) {
-    computer.execute();
-  }
+    halted = computer.execute();
+    paddle = { x: 0, y: 0 };
+    ball = { x: 0, y: 0 };
+    score = 0;
 
-  output.forEach((num, index) => {
-    if ((index + 1) % 3 === 0) {
-      if (num === '2') {
-        numBlocks++;
+    while (computer.output.length > 0) {
+      x = computer.getOutput();
+      y = computer.getOutput();
+      id = computer.getOutput();
+
+      if (x === -1 && y === 0) {
+        score = id;
+        continue;
       }
-    }
-  });
 
-  return numBlocks;
+      if (id === 3) {
+        paddle.x = x;
+        paddle.y = y;
+      }
+
+      if (id === 4) {
+        ball.x = x;
+        ball.y = y;
+      }
+      grid[[x, y]] = id;
+    }
+
+    if (computer.output.length === 0) {
+      print(grid);
+      break;
+    }
+  }
 };
 
 if (require.main === module) {
