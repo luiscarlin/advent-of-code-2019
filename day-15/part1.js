1501632e6d7672d304b8bd37da3174a2df6de012
Luis Carlin <luiscarlin@gmail.com>
2019-12-19 07:18:41 -0500

    start part 1 day 15

--- day-15/part1.js
+++ day-15/part1.js
@@ -1,5 +1,19 @@
 const main = input => {
   return input;
+
+  // input instructions = nswe = 1234 (other ones are invalid)
+  // walks same distance
+
+  // status codes
+  // 0 => hit a wall (no change in position)
+  // 1 => moved one step
+  // 2 => moved one step => new position = position of oxygen system
+
+  while (true) {
+    // pass input for movement
+    // wait for output
+    // output = status on the movement
+  }
 };
 
 if (require.main === module) {
